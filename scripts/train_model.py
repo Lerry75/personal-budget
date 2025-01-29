86ea7544f68657cf693165375be101267bf6dadb
lerry <lerry75@gmail.com>
2025-01-29 18:06:41 +0100

    Logistic Regression and Decision Tree removed. Year removed from features.

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -6,8 +6,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import VotingClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
 import joblib
@@ -23,13 +21,13 @@ def main():
     df['Amount'] = df['Amount'].str.replace(',', '.').astype(float)
 
     # Prepare data
-    X = df[["Notes", "Person", "Amount", "Year", "Month"]]
+    X = df[["Notes", "Person", "Amount", "Month"]]
     y = df['Category']  # target label
 
     # Split data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
-    numeric_features = ["Amount", "Year"]
+    numeric_features = ["Amount"]
     numeric_transformer = StandardScaler()
 
     text_features = "Notes"
@@ -52,14 +50,12 @@ def main():
     # It aggregates multiple base classifiers 
     # and the class with the highest average probability is selected (Soft voting).
     # Define base classifiers
-    clf1 = LogisticRegression(random_state=42)
-    clf2 = DecisionTreeClassifier(random_state=42)
     clf3 = RandomForestClassifier(n_estimators=100, random_state=42)
     clf4 = SVC(probability=True, random_state=42)
 
     # Create VotingClassifier
     voting_clf = VotingClassifier(
-        estimators=[('lr', clf1), ('dt', clf2), ('rfc', clf3), ('svc', clf4)],
+        estimators=[('rfc', clf3), ('svc', clf4)],
         voting='soft'
     )
 
